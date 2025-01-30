907fb772763d6d772803984e4ff9bb0d3af59d0d
Tithuthur <thurthur.petit.py@gmail.com>
2025-01-30 13:40:37 +0100

    Avancé vers la compréhension de l'API

--- request_file.py
+++ request_file.py
@@ -12,8 +12,15 @@ url = "https://api.open-meteo.com/v1/forecast"
 
 # paramettres d'appel envoyés à l'API
 params = {
-    "latitude": 44.63998,
-    "longitude": -0.796953,
-    "hourly": "temperature_2m, relative_humidity_2m"
+    "latitude": 44.58184,#exemple pour plusieurs localisation [44.58184,44]
+    "longitude": -0.039164,#exemple pour plusieurs localisation [-0.039164,-0,04]
+    "hourly": ["temperature_2m","relative_humidity_2m"]
 }
-response = openmeteo.weather_api(url, params=params)
+responses = openmeteo.weather_api(url, params=params)
+
+response1 = responses[0]
+
+print(f"Coordinates {response1.Latitude()}°N {response1.Longitude()}°E")
+print(f"Elevation {response1.Elevation()} m asl")
+print(f"Timezone {response1.Timezone()} {response1.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response1.UtcOffsetSeconds()} s")
